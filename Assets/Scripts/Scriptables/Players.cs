1959088c7d62030f41287b0992c9a6fa69600e68
julitus <julioqq29@gmail.com>
2022-02-07 17:24:24 -0500

    superame init

---
+++ Assets/Scripts/Scriptables/Players.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Player")]
+public class Players : ScriptableObject
+{
+    public GameObject player;
+    public Sprite sprite;
+}
