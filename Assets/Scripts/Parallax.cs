5e3302e37d19dcc00504d34cb13258f17b8caa0e
julitus <julioqq29@gmail.com>
2022-02-11 23:13:06 -0500

    two parallax

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public Transform cam;
+    public float relativeMove = 0.9f;
+    public bool lockY = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lockY) {
+            transform.position = new Vector2(cam.position.x * relativeMove, transform.position.y);
+        } else {
+            transform.position = new Vector2(cam.position.x * relativeMove, cam.position.y * relativeMove);
+        }
+    }
+}
